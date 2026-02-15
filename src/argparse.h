d68cd3a31cd63085058f6681b2b3dd44217baa91
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-15 10:22:06 +0100

    Create argparse.h

---
+++ src/argparse.h
@@ -0,0 +1,79 @@
+/**
+ * argparse.h is a single-header library for parsing command-line arguments in C.
+ *
+ * The goal was to get something relatively extensible and similar to System.CommandLine in .NET.
+ *
+ * Configuration:
+ *  - TODO
+ *
+ * API:
+ *  - TODO
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+// Declaration section                                                        //
+////////////////////////////////////////////////////////////////////////////////
+#ifndef ARGPARSE_H
+#define ARGPARSE_H
+
+#include <stddef.h>
+
+#ifdef ARGPARSE_STATIC
+#   define ARGPARSE_DEF static
+#else
+#   define ARGPARSE_DEF extern
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct OptionDescription {
+    char const* long_name;
+    char const* short_name;
+    char const* description;
+    bool is_required;
+    char const* default_value;
+    bool takes_value;
+} OptionDescription;
+
+typedef struct Option {
+    OptionDescription description;
+    char const* value;
+} Option;
+
+typedef struct CommandDescription {
+    char const* name;
+    char const* description;
+
+    OptionDescription* options;
+    size_t options_length;
+
+    CommandDescription* subcommands;
+    size_t subcommands_length;
+} CommandDescription;
+
+typedef struct ArgumentPack {
+    char const* program_name;
+
+    char const** command_path;
+    size_t command_path_length;
+
+    Option* options;
+    size_t options_length;
+} ArgumentPack;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ARGPARSE_H */
+
+////////////////////////////////////////////////////////////////////////////////
+// Implementation section                                                     //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef ARGPARSE_IMPLEMENTATION
+
+// TODO
+
+#endif /* ARGPARSE_IMPLEMENTATION */
