6985a6addbf8a44b4e7a5c7cf02e31ade5ec9f11
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-15 10:57:09 +0100

    Update argparse.h

--- src/argparse.h
+++ src/argparse.h
@@ -28,7 +28,13 @@
 extern "C" {
 #endif
 
-typedef bool OptionParseFn(char const* text, size_t length, void* result);
+typedef struct OptionParseResult {
+    bool success;
+    void* value;
+    char const* error;
+} OptionParseResult;
+
+typedef OptionParseResult OptionParseFn(char const* text, size_t length);
 
 typedef struct OptionDescription {
     char const* long_name;
@@ -66,6 +72,8 @@ typedef struct ArgumentPack {
     char const* error;
 } ArgumentPack;
 
+ArgumentPack argparse_parse(int argc, char** argv, CommandDescription* root_command);
+
 #ifdef __cplusplus
 }
 #endif
@@ -77,6 +85,47 @@ typedef struct ArgumentPack {
 ////////////////////////////////////////////////////////////////////////////////
 #ifdef ARGPARSE_IMPLEMENTATION
 
-// TODO
+#include <string.h>
+
+ArgumentPack argparse_parse(int argc, char** argv, CommandDescription* root_command) {
+    ArgumentPack result = { 0 };
+
+    if (argc < 1) {
+        result.error = "an empty argument list was provided";
+        return result;
+    }
+
+    result.program_name = argv[0];
+    result.command = root_command;
+
+    int argIndex = 1;
+
+    // Look up the appropriate subcommand
+    while (argIndex < argc) {
+        char const* argPart = argv[argIndex];
+
+        // Look for a subcommand with the given name
+        CommandDescription* matchingCommand = NULL;
+        for (size_t i = 0; i < result.command->subcommands_length; ++i) {
+            if (strcmp(result.command->subcommands[i].name, argPart) == 0) {
+                matchingCommand = &result.command->subcommands[i];
+                break;
+            }
+        }
+
+        // If we found a matching subcommand, move into it and continue parsing
+        if (matchingCommand != NULL) {
+            result.command = matchingCommand;
+            ++argIndex;
+        }
+        else {
+            // Otherwise, we assume that the remaining arguments are options for the current command
+            break;
+        }
+    }
+
+    // We have looked up the appropriate command, now we need to parse the options for it
+    // TODO
+}
 
 #endif /* ARGPARSE_IMPLEMENTATION */
