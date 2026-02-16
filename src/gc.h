9c645f4ba04046fca6fd82c9ccf5a0bb287c3ba3
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-16 22:26:42 +0100

    Update gc.h

--- src/gc.h
+++ src/gc.h
@@ -1,5 +1,5 @@
 /**
- * gc.h is a single-header C garbage collection library.
+ * gc.h is a single-header C mark-and-sweep garbage collection library.
  *
  * It is designed to be easily embeddable into applications.
  *
@@ -46,7 +46,7 @@ typedef struct GC_World {
     // Main data-structure of the opaque allocation elements
     struct {
         struct GC_HashBucket* buckets;
-        size_t buckets_capacity;
+        size_t buckets_length;
         size_t entry_count;
     } hash_map;
 } GC_World;
@@ -82,6 +82,10 @@ GC_DEF void gc_unpin(GC_World* gc, void* mem);
 
 #define GC_ASSERT(condition, message) assert(((void)message, condition))
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 enum : uint8_t {
     GC_FLAG_NONE = 0,
     GC_FLAG_MARKED = 1 << 0,
@@ -89,10 +93,55 @@ enum : uint8_t {
 };
 
 typedef struct GC_Allocation {
-    void* base;
+    void* base_address;
     size_t size;
     uint8_t flags;
 } GC_Allocation;
+
+// Platform-specific ///////////////////////////////////////////////////////////
+
+#if defined(_WIN32)
+#include <Windows.h>
+#elif defined(__linux__) || defined(__APPLE__)
+#include <pthread.h>
+#endif
+
+void* gc_platform_compute_stack_bottom() {
+#if defined(_WIN32)
+    ULONG_PTR low, high;
+    GetCurrentThreadStackLimits(&low, &high);
+    return (void*)low;
+#elif defined(__linux__) || defined(__APPLE__)
+    pthread_attr_t attr;
+    pthread_getattr_np(pthread_self(), &attr);
+    size_t stack_size;
+    void* stack_addr;
+    pthread_attr_getstack(&attr, &stack_addr, &stack_size);
+    void* stack_bottom = (char*)stack_addr + stack_size; // top of stack memory
+    pthread_attr_destroy(&attr);
+    return stack_bottom;
+#else
+    #error "unsupported platform"
+#endif
+}
+
+#if defined(_WIN32)
+    // TODO
+#elif defined(__linux__) || defined(__APPLE__)
+    extern char __data_start;
+    extern char _end;
+#endif
+
+void gc_platform_get_global_section_range(void** start, void** end) {
+#if defined(_WIN32)
+    // TODO
+#elif defined(__linux__) || defined(__APPLE__)
+    *start = (void*)__data_start;
+    *end = (void*)_end;
+#else
+    #error "unsupported platform"
+#endif
+}
 
 // Hash-map ////////////////////////////////////////////////////////////////////
 
@@ -117,7 +166,7 @@ static uintptr_t gc_hash_code(void* address) {
 static double gc_hash_map_load_factor(GC_World* gc) {
     // We handle NULL buckets as max load factor
     if (gc->hash_map.buckets == NULL) return 1;
-    return (double)gc->hash_map.entry_count / gc->hash_map.buckets_capacity;
+    return (double)gc->hash_map.entry_count / gc->hash_map.buckets_length;
 }
 
 static void gc_add_to_hash_bucket(GC_HashBucket* bucket, GC_HashEntry entry) {
@@ -136,20 +185,20 @@ static void gc_remove_from_hash_bucket_at(GC_HashBucket* bucket, size_t index) {
     memmove(bucket->entries + index, bucket->entries + index + 1, sizeof(GC_HashEntry) * (bucket->length - index));
 }
 
-static void gc_resize_hash_map_to_number_of_buckets(GC_World* gc, size_t newCapacity) {
-    if (gc->hash_map.buckets_capacity == newCapacity) return;
+static void gc_resize_hash_map_to_number_of_buckets(GC_World* gc, size_t newLength) {
+    if (gc->hash_map.buckets_length == newLength) return;
     // Allocate a new bucket array
-    GC_HashBucket* newBuckets = (GC_HashBucket*)GC_REALLOC(NULL, sizeof(GC_HashBucket) * newCapacity);
+    GC_HashBucket* newBuckets = (GC_HashBucket*)GC_REALLOC(NULL, sizeof(GC_HashBucket) * newLength);
     GC_ASSERT(newBuckets != NULL, "failed to allocate bucket array for GC hash map upscaling");
     // Initialize the new bucket arrays
-    memset(newBuckets, 0, sizeof(GC_HashBucket) * newCapacity);
+    memset(newBuckets, 0, sizeof(GC_HashBucket) * newLength);
     // Add each item from each bucket to the new bucket array, essentially redistributing
-    for (size_t i = 0; i < gc->hash_map.buckets_capacity; ++i) {
+    for (size_t i = 0; i < gc->hash_map.buckets_length; ++i) {
         GC_HashBucket* oldBucket = &gc->hash_map.buckets[i];
         for (size_t j = 0; j < oldBucket->length; ++j) {
             GC_HashEntry entry = oldBucket->entries[j];
             // Compute the new bucket index
-            size_t newBucketIndex = entry.hash_code % newCapacity;
+            size_t newBucketIndex = entry.hash_code % newLength;
             gc_add_to_hash_bucket(&newBuckets[newBucketIndex], entry);
         }
         // The old bucket's elements have been redistributed, free it up
@@ -158,47 +207,47 @@ static void gc_resize_hash_map_to_number_of_buckets(GC_World* gc, size_t newCapa
     // Free the old bucket, replace entries
     GC_FREE(gc->hash_map.buckets);
     gc->hash_map.buckets = newBuckets;
-    gc->hash_map.buckets_capacity = newCapacity;
+    gc->hash_map.buckets_length = newLength;
 }
 
 static void gc_grow_hash_map(GC_World* gc) {
     // Let's double the size of the bucket array
-    size_t newCapacity = gc->hash_map.buckets_capacity * 2;
-    if (newCapacity < 8) newCapacity = 8;
-    gc_resize_hash_map_to_number_of_buckets(gc, newCapacity);
+    size_t newLength = gc->hash_map.buckets_length * 2;
+    if (newLength < 8) newLength = 8;
+    gc_resize_hash_map_to_number_of_buckets(gc, newLength);
 }
 
 static void gc_shrink_hash_map(GC_World* gc) {
     // Lets halve the size of the bucket array
-    size_t newCapacity = gc->hash_map.buckets_capacity / 2;
-    if (newCapacity < 8) return;
-    gc_resize_hash_map_to_number_of_buckets(gc, newCapacity);
+    size_t newLength = gc->hash_map.buckets_length / 2;
+    if (newLength < 8) return;
+    gc_resize_hash_map_to_number_of_buckets(gc, newLength);
 }
 
 static void gc_add_allocation_to_hash_map(GC_World* gc, GC_Allocation allocation) {
     // Grow if needed
     double loadFactor = gc_hash_map_load_factor(gc);
-    if (loadFactor > GC_HashTable_UpsizeLoadFactor || gc->hash_map.buckets_capacity == 0) gc_grow_hash_map(gc);
+    if (loadFactor > GC_HashTable_UpsizeLoadFactor || gc->hash_map.buckets_length == 0) gc_grow_hash_map(gc);
 
     // Emplace
     GC_HashEntry entry = {
         .allocation = allocation,
-        .hash_code = gc_hash_code(allocation.base),
+        .hash_code = gc_hash_code(allocation.base_address),
     };
-    size_t bucketIndex = entry.hash_code % gc->hash_map.buckets_capacity;
+    size_t bucketIndex = entry.hash_code % gc->hash_map.buckets_length;
     gc_add_to_hash_bucket(&gc->hash_map.buckets[bucketIndex], entry);
     ++gc->hash_map.entry_count;
 }
 
 static void gc_remove_allocation_from_hash_map(GC_World* gc, void* baseAddress) {
-    if (gc->hash_map.buckets_capacity == 0) return;
+    if (gc->hash_map.buckets_length == 0) return;
 
     // First, compute what bucket the element would be in
-    size_t bucketIndex = gc_hash_code(baseAddress) % gc->hash_map.buckets_capacity;
+    size_t bucketIndex = gc_hash_code(baseAddress) % gc->hash_map.buckets_length;
     // Look for the index within the bucket
     GC_HashBucket* bucket = &gc->hash_map.buckets[bucketIndex];
     for (size_t i = 0; i < bucket->length; ++i) {
-        if (bucket->entries[i].allocation.base == baseAddress) {
+        if (bucket->entries[i].allocation.base_address == baseAddress) {
             // Found
             gc_remove_from_hash_bucket_at(bucket, i);
             --gc->hash_map.entry_count;
@@ -212,23 +261,33 @@ static void gc_remove_allocation_from_hash_map(GC_World* gc, void* baseAddress) 
 }
 
 static GC_Allocation* gc_get_allocation_from_hash_map(GC_World* gc, void* baseAddress) {
-    if (gc->hash_map.buckets_capacity == 0) return NULL;
+    if (gc->hash_map.buckets_length == 0) return NULL;
 
     // First, compute what bucket the element would be in
-    size_t bucketIndex = gc_hash_code(baseAddress) % gc->hash_map.buckets_capacity;
+    size_t bucketIndex = gc_hash_code(baseAddress) % gc->hash_map.buckets_length;
      // Search within bucket
     GC_HashBucket* bucket = &gc->hash_map.buckets[bucketIndex];
     for (size_t i = 0; i < bucket->length; ++i) {
         GC_Allocation* allocation = &bucket->entries[i].allocation;
         // Found
-        if (allocation->base == baseAddress) return allocation;
+        if (allocation->base_address == baseAddress) return allocation;
     }
 
     // Not found
     return NULL;
 }
 
-// Mark and sweep //////////////////////////////////////////////////////////////
+// Mark ////////////////////////////////////////////////////////////////////////
+
+static void* gc_align_address_forward(void* address) {
+    uintptr_t misalign = (uintptr_t)address % sizeof(uintptr_t);
+    if (misalign > 0) address += sizeof(uintptr_t) - misalign;
+    return address;
+}
+
+static void* gc_align_address_backward(void* address) {
+    return address - (uintptr_t)address % sizeof(uintptr_t);
+}
 
 static void gc_mark_address(GC_World* gc, void* addr) {
     GC_Allocation* allocation = gc_get_allocation_from_hash_map(gc, addr);
@@ -239,12 +298,11 @@ static void gc_mark_address(GC_World* gc, void* addr) {
     // First off, mark
     allocation->flags |= GC_FLAG_MARKED;
     // Then we can mark each address within this allocated region
-    uint8_t* startAddress = (uint8_t*)allocation->base;
+    uint8_t* startAddress = (uint8_t*)allocation->base_address;
     // NOTE: We make sure to only read addresses fully within
-    uint8_t* endAddress = startAddress + allocation->size - (allocation->size % sizeof(uintptr_t));
+    uint8_t* endAddress = gc_align_address_backward(startAddress + allocation->size);
     // Align start address
-    uintptr_t misalign = (uintptr_t)startAddress % sizeof(uintptr_t);
-    if (misalign > 0) startAddress += sizeof(uintptr_t) - misalign;
+    startAddress = (uint8_t*)gc_align_address_forward(startAddress);
     // Mark every address referenced within
     for (uintptr_t* addr = (uintptr_t*)startAddress; addr < (uintptr_t*)endAddress; ++addr) {
         void* referencedAddr = (void*)*addr;
@@ -252,11 +310,34 @@ static void gc_mark_address(GC_World* gc, void* addr) {
     }
 }
 
+static void gc_mark_pinned(GC_World* gc) {
+    // Simply enumerate the allocations and mark everything with the pinned flag
+    for (size_t i = 0; i < gc->hash_map.buckets_length; ++i) {
+        GC_HashBucket* bucket = &gc->hash_map.buckets[i];
+        for (size_t j = 0; j < bucket->length; ++j) {
+            GC_Allocation* allocation = &bucket->entries[j].allocation;
+            if ((allocation->flags & GC_FLAG_PINNED) != 0) gc_mark_address(gc, allocation->base_address);
+        }
+    }
+}
+
+static void gc_mark_stack(GC_World* gc) {
+    // TODO: spill registers and walk through stack
+}
+
+static void gc_mark_globals(GC_World* gc) {
+    // TODO: go through the globals section
+}
+
 static void gc_mark(GC_World* gc) {
     gc_mark_pinned(gc);
     gc_mark_stack(gc);
     gc_mark_globals(gc);
 }
+
+#ifdef __cplusplus
+}
+#endif
 
 #undef GC_ASSERT
 
