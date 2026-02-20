d2cd42565fec4f82292967b6db84cc8125719463
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-20 20:39:58 +0100

    Update arena.h

--- src/arena.h
+++ src/arena.h
@@ -14,6 +14,8 @@
 #ifndef ARENA_H
 #define ARENA_H
 
+#include <stddef.h>
+
 #ifdef ARENA_STATIC
 #   define ARENA_DEF static
 #else
@@ -31,7 +33,35 @@
 extern "C" {
 #endif
 
-// TODO
+struct Arena_Block;
+
+typedef enum Area_Growth {
+    // Each block is the same size
+    AREA_GROWTH_UNIFORM,
+    // Each block is double the size of the previous block
+    AREA_GROWTH_DOUBLE,
+} Area_Growth;
+
+typedef struct Arena {
+    struct Arena_Block* block;
+    size_t block_count;
+    size_t block_size;
+    size_t max_block_size;
+    size_t large_threshold;
+    Area_Growth growth;
+} Arena;
+
+typedef struct Arena_Mark {
+    struct Arena_Block* block;
+    size_t block_count;
+    size_t offset;
+} Arena_Mark;
+
+ARENA_DEF void* arena_alloc(Arena* arena, size_t size);
+ARENA_DEF void* arena_alloc_aligned(Arena* arena, size_t size, size_t alignment);
+ARENA_DEF void arena_destroy(Arena* arena);
+ARENA_DEF Arena_Mark arena_mark(Arena* arena);
+ARENA_DEF void arena_reset(Arena* arena, Arena_Mark mark);
 
 #ifdef __cplusplus
 }
