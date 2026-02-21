7dd6eceadb3d9af42fc321d6c83e036ceab631c0
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-21 13:22:05 +0100

    Create string_builder.h

---
+++ src/string_builder.h
@@ -0,0 +1,73 @@
+/**
+ * string_builder.h is a single-header string building library for C.
+ *
+ * Configuration:
+ *  - TODO
+ *
+ * API:
+ *  - TODO
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+// Declaration section                                                        //
+////////////////////////////////////////////////////////////////////////////////
+#ifndef STRING_BUILDER_H
+#define STRING_BUILDER_H
+
+// TODO: Includes
+
+// TODO: Configuration macros
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO: API declarations
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* STRING_BUILDER_H */
+
+////////////////////////////////////////////////////////////////////////////////
+// Implementation section                                                     //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef STRING_BUILDER_IMPLEMENTATION
+
+// TODO: Includes
+
+// TODO: Local helper macro definitions
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO: implementation
+
+#ifdef __cplusplus
+}
+#endif
+
+// TODO: Cleanup local helper macro definitions
+
+#endif /* STRING_BUILDER_IMPLEMENTATION */
+
+////////////////////////////////////////////////////////////////////////////////
+// Self-testing section                                                       //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef STRING_BUILDER_SELF_TEST
+
+// TODO
+
+#endif /* STRING_BUILDER_SELF_TEST */
+
+////////////////////////////////////////////////////////////////////////////////
+// Example section                                                            //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef STRING_BUILDER_EXAMPLE
+#undef STRING_BUILDER_EXAMPLE
+
+// TODO
+
+#endif /* STRING_BUILDER_EXAMPLE */
