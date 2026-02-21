50e8f9b4c1d0b9166650f32e48254dfae3b5a717
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-21 14:15:12 +0100

    Update template_generator.c

--- tools/template_generator.c
+++ tools/template_generator.c
@@ -2,6 +2,10 @@
 #define ARGPARSE_STATIC
 #include "../src/argparse.h"
 
+#define STRING_BUILDER_IMPLEMENTATION
+#define STRING_BUILDER_STATIC
+#include "../src/string_builder.h"
+
 static int generate_template() {
 
 }
