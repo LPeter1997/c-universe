925700ebe413a7886ac09be1bb632df010cd6e32
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-21 17:29:49 +0100

    Update json.h

--- src/json.h
+++ src/json.h
@@ -16,6 +16,7 @@
 #ifndef JSON_H
 #define JSON_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
 #ifdef JSON_STATIC
@@ -35,7 +36,104 @@
 extern "C" {
 #endif
 
-// TODO: Public API declarations
+typedef enum Json_Extension {
+    JSON_EXTENSION_NONE = 0,
+    JSON_EXTENSION_COMMENTS = 1 << 0,
+    JSON_EXTENSION_TRAILING_COMMAS = 1 << 1,
+} Json_Extension;
+
+typedef struct Json_Options {
+    Json_Extension extensions;
+    char const* newline_str;
+    char const* indent_str;
+} Json_Options;
+
+typedef struct Json_Error {
+    char const* message;
+    size_t line;
+    size_t column;
+    size_t index;
+} Json_Error;
+
+typedef struct Json_Sax {
+    void(*on_null)(void* user_data);
+    void(*on_bool)(void* user_data, bool value);
+    void(*on_int)(void* user_data, long long value);
+    void(*on_double)(void* user_data, double value);
+    void(*on_string)(void* user_data, char const* value, size_t length);
+    void(*on_array_start)(void* user_data);
+    void(*on_array_end)(void* user_data);
+    void(*on_object_start)(void* user_data);
+    void(*on_object_key)(void* user_data, char const* key, size_t length);
+    void(*on_object_end)(void* user_data);
+    void(*on_error)(void* user_data, Json_Error error);
+} Json_Sax;
+
+typedef enum Json_ValueType {
+    JSON_VALUE_NULL,
+    JSON_VALUE_BOOL,
+    JSON_VALUE_INT,
+    JSON_VALUE_DOUBLE,
+    JSON_VALUE_STRING,
+    JSON_VALUE_ARRAY,
+    JSON_VALUE_OBJECT,
+} Json_ValueType;
+
+typedef struct Json_Value {
+    Json_ValueType type;
+    union {
+        bool bool_value;
+        long long int_value;
+        double double_value;
+        char const* string_value;
+        struct {
+            Json_Value* elements;
+            size_t length;
+            size_t capacity;
+        } array_value;
+        struct {
+            char const** keys;
+            Json_Value* values;
+            size_t length;
+            size_t capacity;
+        } object_value;
+    };
+} Json_Value;
+
+typedef struct Json_Document {
+    Json_Value root;
+    struct {
+        Json_Error* elements;
+        size_t length;
+        size_t capacity;
+    } errors;
+} Json_Document;
+
+JSON_DEF void json_parse_sax(char const* json, Json_Sax sax, void* user_data, Json_Options options);
+JSON_DEF Json_Document json_parse(char const* json, Json_Options options);
+JSON_DEF size_t json_serialize_to(Json_Value value, Json_Options options, char* buffer, size_t buffer_size);
+JSON_DEF char* json_serialize(Json_Value value, Json_Options options, size_t* out_length);
+
+JSON_DEF Json_Value json_object(void);
+JSON_DEF Json_Value json_array(void);
+JSON_DEF Json_Value json_string(char const* str);
+JSON_DEF Json_Value json_int(long long value);
+JSON_DEF Json_Value json_double(double value);
+JSON_DEF Json_Value json_bool(bool value);
+JSON_DEF Json_Value json_null(void);
+
+JSON_DEF void json_value_free(Json_Value* value);
+JSON_DEF void json_document_free(Json_Document* doc);
+
+JSON_DEF void json_object_set(Json_Value* object, char const* key, Json_Value value);
+JSON_DEF Json_Value* json_object_get(Json_Value* object, char const* key);
+JSON_DEF bool json_object_get_at(Json_Value* object, size_t index, char const** out_key, Json_Value* out_value);
+JSON_DEF bool json_object_remove(Json_Value* object, char const* key);
+
+JSON_DEF void json_array_append(Json_Value* array, Json_Value value);
+JSON_DEF void json_array_set(Json_Value* array, size_t index, Json_Value value);
+JSON_DEF Json_Value* json_array_get(Json_Value* array, size_t index);
+JSON_DEF void json_array_remove(Json_Value* array, size_t index);
 
 #ifdef __cplusplus
 }
