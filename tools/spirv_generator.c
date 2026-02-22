d9b24b304274212537287342889b047dc9acb5ea
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-22 22:30:03 +0100

    Yes

--- tools/spirv_generator.c
+++ tools/spirv_generator.c
@@ -5,7 +5,7 @@
 #include "../src/json.h"
 
 // TODO: It would be very nice if a library provided easier IO functions that could let me do all this in one go
-char const* read_file(char const* path) {
+static char const* read_file(char const* path) {
     FILE* file = fopen(path, "rb");
     if (!file) return NULL;
     fseek(file, 0, SEEK_END);
@@ -22,6 +22,33 @@ char const* read_file(char const* path) {
     return content;
 }
 
+static void json_model_to_domain(Json_Document doc) {
+    // TODO: Would be nice to have a generic sorting algo at hand, instead to put all composite types at the end,
+    // we remove them, collect them out to a separate list, then add them back at the end
+    Json_Value* operandKinds = json_object_get(&doc.root, "operand_kinds");
+    DynamicArray(Json_Value) compositeTypes = {0};
+    // TODO: Lackluster API, accessing array length should not involve diving into the guts of the union
+    // Also makes it EXTREMELY easy to cause UB as the access is not validated
+    for (size_t i = 0; i < operandKinds->value.array.length; ) {
+        Json_Value* operandKind = json_array_get(operandKinds, i);
+        Json_Value* category = json_object_get(operandKind, "category");
+        // TODO: Same here, maybe add asserting accessors?
+        if (strcmp(category->value.string, "Composite") != 0) {
+            ++i;
+            continue;
+        }
+        // TODO: We have no way to MOVE a value out of the structures without deallocating them
+        // We should consider fixing the API
+        DynamicArray_append(compositeTypes, json_copy(*operandKind));
+        json_array_remove(operandKinds, i);
+    }
+    for (size_t i = 0; i < DynamicArray_length(compositeTypes); ++i) {
+        Json_Value operandKind = DynamicArray_at(compositeTypes, i);
+        json_array_append(operandKinds, operandKind);
+    }
+    DynamicArray_free(compositeTypes);
+}
+
 int main(void) {
     char const* jsonStr = read_file("../third_party/spirv.core.grammar.json");
     if (!jsonStr) {
@@ -35,8 +62,9 @@ int main(void) {
         json_free_document(&doc);
         return 1;
     }
-    // As a test we just serialize back and dump
-    char* output = json_write(doc.root, (Json_Options){ .indent_str = "  ", .newline_str = "\n" }, NULL);
+    json_model_to_domain(doc);
+    // For now this just manipulates the JSON, reprint to check
+    char* output = json_write(doc.root, (Json_Options){ .newline_str = "\n", .indent_str = "  " }, NULL);
     printf("%s\n", output);
     free(output);
     json_free_document(&doc);
