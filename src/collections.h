dd7be9a26ff52130e0fc9d78e0577d83745d640e
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-22 16:09:59 +0100

    Update collections.h

--- src/collections.h
+++ src/collections.h
@@ -16,7 +16,10 @@
 #ifndef COLLECTIONS_H
 #define COLLECTIONS_H
 
+#include <assert.h>
 #include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
 
 #ifdef COLLECTIONS_STATIC
     #define COLLECTIONS_DEF static
@@ -30,6 +33,63 @@
 #ifndef COLLECTIONS_FREE
     #define COLLECTIONS_FREE free
 #endif
+
+// Dynamic array ///////////////////////////////////////////////////////////////
+
+#ifndef COLLECTIONS_ASSERT
+    #define COLLECTIONS_ASSERT(condition, message) assert(((void)message, condition))
+#endif
+
+#define DynamicArray(T) \
+    struct { \
+        T* elements; \
+        size_t length; \
+        size_t capacity; \
+    }
+
+#define DynamicArray_free(array) \
+    do { \
+        COLLECTIONS_FREE((array).elements); \
+        (array).elements = NULL; \
+        (array).length = 0; \
+        (array).capacity = 0; \
+    } while (false)
+
+#define DynamicArray_at(array, index) (array).elements[(index)]
+
+#define DynamicArray_reserve(array, new_capacity) \
+    do { \
+        if ((new_capacity) > (array).capacity) { \
+            size_t newCap = (array).capacity == 0 ? 8 : (array).capacity * 2; \
+            while (newCap < (new_capacity)) newCap *= 2; \
+            void* newElements = COLLECTIONS_REALLOC((array).elements, newCap * sizeof(*(array).elements)); \
+            COLLECTIONS_ASSERT(newElements != NULL, "failed to allocate memory for dynamic array"); \
+            (array).elements = newElements; \
+            (array).capacity = newCap; \
+        } \
+    } while (false)
+
+#define DynamicArray_append(array, element) \
+    do { \
+        DynamicArray_reserve((array), (array).length + 1); \
+        (array).elements[(array).length++] = (element); \
+    } while (false)
+
+#define DynamicArray_insert(array, index, element) \
+    do { \
+        COLLECTIONS_ASSERT((index) <= (array).length, "index out of bounds for dynamic array insertion"); \
+        DynamicArray_reserve((array), (array).length + 1); \
+        memmove(&(array).elements[(index) + 1], &(array).elements[index], ((array).length - (index)) * sizeof(*(array).elements)); \
+        (array).elements[index] = (element); \
+        ++(array).length; \
+    } while (false)
+
+#define DynamicArray_remove(array, index) \
+    do { \
+        COLLECTIONS_ASSERT((index) < (array).length, "index out of bounds for dynamic array removal"); \
+        memmove(&(array).elements[index], &(array).elements[(index) + 1], ((array).length - (index) - 1) * sizeof(*(array).elements)); \
+        --(array).length; \
+    } while (false)
 
 #ifdef __cplusplus
 extern "C" {
