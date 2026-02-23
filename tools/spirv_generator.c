a7e4bfeb120d947d85b9508d7e6c49eafaa46c96
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-23 19:05:33 +0100

    Update spirv_generator.c

--- tools/spirv_generator.c
+++ tools/spirv_generator.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "../src/collections.h"
 
 #define JSON_IMPLEMENTATION
@@ -23,6 +25,7 @@ static char* read_file(char const* path) {
 }
 
 static void json_model_to_domain(Json_Document doc) {
+    // Reorder operand_kinds to have Composite at the end
     // TODO: Would be nice to have a generic sorting algo at hand, instead to put all composite types at the end,
     // we remove them, collect them out to a separate list, then add them back at the end
     Json_Value* operandKinds = json_object_get(&doc.root, "operand_kinds");
@@ -42,6 +45,33 @@ static void json_model_to_domain(Json_Document doc) {
         json_array_append(operandKinds, json_move(operandKind));
     }
     DynamicArray_free(compositeTypes);
+
+    // We go through each enumerant of each operand_kind and if it has an "aliases" array, we unroll it by
+    // cloning the enumerant for each alias and replacing the name with the alias
+    for (size_t i = 0; i < json_length(operandKinds); ++i) {
+        Json_Value* operandKind = json_array_at(operandKinds, i);
+        Json_Value* enumerants = json_object_get(operandKind, "enumerants");
+        if (enumerants == NULL) continue;
+        DynamicArray(Json_Value) newEnumerants = {0};
+        for (size_t j = 0; j < json_length(enumerants); ++j) {
+            Json_Value* enumerant = json_array_at(enumerants, j);
+            Json_Value* aliases = json_object_get(enumerant, "aliases");
+            if (aliases == NULL) continue;
+            for (size_t k = 0; k < json_length(aliases); ++k) {
+                Json_Value* alias = json_array_at(aliases, k);
+                Json_Value newEnumerant = json_copy(*enumerant);
+                json_object_remove(&newEnumerant, "aliases", NULL);
+                json_object_set(&newEnumerant, "name", json_move(alias));
+                DynamicArray_append(newEnumerants, json_move(&newEnumerant));
+            }
+            json_object_remove(enumerant, "aliases", NULL);
+        }
+        for (size_t j = 0; j < DynamicArray_length(newEnumerants); ++j) {
+            Json_Value* newEnumerant = &DynamicArray_at(newEnumerants, j);
+            json_array_append(enumerants, json_move(newEnumerant));
+        }
+        DynamicArray_free(newEnumerants);
+    }
 }
 
 int main(void) {
