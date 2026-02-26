69d0dbf3a614cc0c22230a3727896ca7750c0261
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-26 16:09:24 +0100

    Create spv.h

---
+++ src/spv.h
@@ -0,0 +1,96 @@
+/**
+ * spv.h is a single-header library for generating SPIR-V binary code.
+ *
+ * Configuration:
+ *  - TODO
+ *
+ * API:
+ *  - TODO
+ *
+ * Check the example section at the end of this file for a full example.
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+// Declaration section                                                        //
+////////////////////////////////////////////////////////////////////////////////
+#ifndef SPV_H
+#define SPV_H
+
+#include <stddef.h>
+
+#ifdef SPV_STATIC
+    #define SPV_DEF static
+#else
+    #define SPV_DEF extern
+#endif
+
+#ifndef SPV_ASSERT
+    #define SPV_ASSERT(condition, message) ((void)message, (condition))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO: Public API declarations
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SPV_H */
+
+////////////////////////////////////////////////////////////////////////////////
+// Implementation section                                                     //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef SPV_IMPLEMENTATION
+
+#include <assert.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO: Implementation goes here
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SPV_IMPLEMENTATION */
+
+////////////////////////////////////////////////////////////////////////////////
+// Self-testing section                                                       //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef SPV_SELF_TEST
+
+// Use our own testing library for self-testing
+#define CTEST_STATIC
+#define CTEST_IMPLEMENTATION
+#define CTEST_MAIN
+#include "ctest.h"
+
+CTEST_CASE(sample_test) {
+    CTEST_ASSERT_FAIL("TODO");
+}
+
+#endif /* SPV_SELF_TEST */
+
+////////////////////////////////////////////////////////////////////////////////
+// Example section                                                            //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef SPV_EXAMPLE
+#undef SPV_EXAMPLE
+
+#include <stdio.h>
+
+#define SPV_IMPLEMENTATION
+#define SPV_STATIC
+#include "spv.h"
+
+int main(void) {
+    // TODO: Example usage goes here
+    return 0;
+}
+
+#endif /* SPV_EXAMPLE */
