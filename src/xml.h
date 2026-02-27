3c3051250ad682b578e334be83295b14f7d1f5fa
LPeter1997 <lenkefi.peti@gmail.com>
2026-02-27 21:23:49 +0100

    Create xml.h

---
+++ src/xml.h
@@ -0,0 +1,96 @@
+/**
+ * xml.h is a single-header TODO.
+ *
+ * Configuration:
+ *  - TODO
+ *
+ * API:
+ *  - TODO
+ *
+ * Check the example section at the end of this file for a full example.
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+// Declaration section                                                        //
+////////////////////////////////////////////////////////////////////////////////
+#ifndef XML_H
+#define XML_H
+
+#include <stddef.h>
+
+#ifdef XML_STATIC
+    #define XML_DEF static
+#else
+    #define XML_DEF extern
+#endif
+
+#ifndef XML_ASSERT
+    #define XML_ASSERT(condition, message) ((void)message, (condition))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO: Public API declarations
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XML_H */
+
+////////////////////////////////////////////////////////////////////////////////
+// Implementation section                                                     //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef XML_IMPLEMENTATION
+
+#include <assert.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO: Implementation goes here
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XML_IMPLEMENTATION */
+
+////////////////////////////////////////////////////////////////////////////////
+// Self-testing section                                                       //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef XML_SELF_TEST
+
+// Use our own testing library for self-testing
+#define CTEST_STATIC
+#define CTEST_IMPLEMENTATION
+#define CTEST_MAIN
+#include "ctest.h"
+
+CTEST_CASE(sample_test) {
+    CTEST_ASSERT_FAIL("TODO");
+}
+
+#endif /* XML_SELF_TEST */
+
+////////////////////////////////////////////////////////////////////////////////
+// Example section                                                            //
+////////////////////////////////////////////////////////////////////////////////
+#ifdef XML_EXAMPLE
+#undef XML_EXAMPLE
+
+#include <stdio.h>
+
+#define XML_IMPLEMENTATION
+#define XML_STATIC
+#include "xml.h"
+
+int main(void) {
+    // TODO: Example usage goes here
+    return 0;
+}
+
+#endif /* XML_EXAMPLE */
